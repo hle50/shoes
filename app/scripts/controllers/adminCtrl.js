09761165f60904ce8b97599821420f32bcc96612
hoale <ledanghoa1984@outlook.com>
2016-02-27 08:46:47 +0700

    refactor

--- app/scripts/controllers/adminCtrl.js
+++ app/scripts/controllers/adminCtrl.js
@@ -10,15 +10,20 @@
 angular.module('slimApp')
   .controller('AdminCtrl', ['categoryFactory', '$scope', function (categoryFactory, $scope) {
 
-    $scope.page ={
+    $scope.page = {
       currentPage: 1,
       pageSize: 10,
-      filter:'',
-      options: [10,15,20]
+      filter: '',
+      options: [10, 15, 20]
     };
+    $scope.isConfig = false;
+    $scope.sortType = 'categoryName'; // set the default sort type
+    $scope.sortReverse = false;  // set the default sort order
+
     function getAllCategory() {
       categoryFactory.getAllCategory().then(function (resp) {
         $scope.category = resp.data.categories;
+        $scope.categoryCopy = angular.copy($scope.category);
       })
     }
 
@@ -28,8 +33,8 @@ angular.module('slimApp')
       order: ''
     };
 
-    $scope.saveCategory =function(){
-      categoryFactory.createCategory($scope.formData).then(function(resp){
+    $scope.saveCategory = function () {
+      categoryFactory.createCategory($scope.formData).then(function (resp) {
         getAllCategory();
       })
     };
